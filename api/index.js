61b1e539fb4d43fc96ae38df703b231e1527db1f
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2023-10-25 23:10:25 +0100

    upload photo
    upload photo functionality on new places been added

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'hfjskwnrcdjakdqoihf'
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'))
 
 app.use(cors({
   credentials:true,
@@ -88,12 +89,12 @@ app.post('/logout', (req, res)=>{
 
 app.post('/upload-by-link', async(req, res)=>{
   const {link} = req.body;
-  const newName = Date.now() + '.jpg';
+  const newName = 'photo' + Date.now() + '.jpg';
   await imageDownloader.image({
     url: link,
-    dest: __dirname + '/uploads' +newName,
+    dest: __dirname + '/uploads/' +newName,
   });
-
+  res.json(newName)
 
 })
 
